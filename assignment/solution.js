3be68f4a913cc99f79d69e4fbf0ff6f521c3d06a
Jomana Sherif <jomana.mesbah@student.giu-uni.de>
2026-09-22 21:33:35 +0300

    Add solution

---
+++ assignment/solution.js
@@ -0,0 +1,8 @@
+const heroName = "Jomana";
+const studentId = 16001137;
+const power = studentId * 6;
+
+console.log(heroName);
+console.log(studentId);
+console.log(power);
+console.log("By Alexandria, the Sand Kraken shatters!");
